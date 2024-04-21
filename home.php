b70a5a11abba6bfe1cd35dd219a73a07e7be5b97
AdvaitKrishna <80826027+AdvaitKrishna@users.noreply.github.com>
2024-04-21 14:33:41 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,35 @@
+<!-- <h1><?php echo $title;?></h1> -->
+
+<div class="col-lg-12">  
+<h4>Mandate</h4>
+    <p> we have built an online web appliation(S P A R K) for students of age 5-12. We are making a website to help these children reach their development milestone During 
+        the current situation of pandemic, their learning system has been affected, so by several online meeting their learning may not help, in that case, 
+        this website will help overcome these problems. </p> 
+</div>
+<div  class="col-lg-6">
+  <h4>Vision</h4> 
+  <p>Our goal is the overall development of kids with special needs.During the learning phase, they face various type of problems.Various websites are out there, but they are only made for
+     the normal kids.We still do not have any website for kids with special needs.We will be working for special children who are in the age group of 5-12.</p>
+  <h4>Mission</h4>
+  <p> A good foundation in the early years makes a difference through adulthood and even gives the next generation a better start. Educated and healthy people participate in, and contribute
+      to, the financial and social wealth of their societies.</p>
+  <h4>Core Values</h4> 
+  <ol>
+    <li>Pursuit of TRUTH.</li>  
+    <li>Passion for EXCELLENCE</li> 
+    <li>Commitment to SERVICE</li> 
+  </ol>
+
+
+</div>
+<div  class="col-lg-6">
+  <h4>Guiding Principles</h4> 
+    <ul>
+      <li>Academic Freedom </li>
+      <li>Responsibility</li>
+      <li>Academic Standards</li>  
+    </ul>
+
+</div>
+ 
+
